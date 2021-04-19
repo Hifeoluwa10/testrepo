cb048c3792029539da2d99ed253237960bce044b
Hifeoluwa10 <bbtunde17@gmail.com>
2021-04-19 09:24:55 +0100

    committing edited file

--- info.py
+++ info.py
@@ -1,6 +1,9 @@
 print("Hello World!")
 print("need to get great at this thing")
+print("its possible, absolutely
+print("need to get great at this thing")
 print("its possible, absolutely!")
+
 
 #show some grit , attitude and tenacity
 #keep your end down, work , work work.
