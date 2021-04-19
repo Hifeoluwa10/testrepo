edbffeeeae99cf3843a093fbfc9d05befa7f51c5
Hifeoluwa10 <bbtunde17@gmail.com>
2021-04-19 09:17:34 +0100

    commiting info.py

---
+++ info.py
@@ -0,0 +1,6 @@
+print("Hello World!")
+print("need to get great at this thing")
+print("its possible, absolutely!")
+
+#show some grit , attitude and tenacity
+#keep your end down, work , work work.
